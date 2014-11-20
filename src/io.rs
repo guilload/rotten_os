117db77091b07727651bb09c10de969a70c0ddea
Adrien Guillo <adrien.guillo@gmail.com>
2014-11-20 12:23:03 -0500

    Read from port

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,13 @@
 pub mod port {
 
+    pub fn read(port: u16) -> u8 {
+        unsafe {
+            let byte: u8;
+            asm!("inb %dx, %al" : "={al}" (byte) : "{dx}" (port) :: "volatile");
+            byte
+        }
+    }
+
     pub fn write(port: u16, value: u8) {
         unsafe {
             asm!("outb %al, %dx" :: "{dx}" (port), "{al}" (value) :: "volatile");
